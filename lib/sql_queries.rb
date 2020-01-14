b7bf590a3e7d52b767ff35e1985ebae5eddbc573
Mark Harvey <maharvey2@crimson.ua.edu>
2020-01-14 15:47:41 -0500

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,13 +19,14 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Select Distinct color, Count(Distinct color) As Count From bears;"
+  "Select color, Count(color) from bears Group By color Order by Count(*) DESC Limit 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "Select Count(temperament) from bears Where temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT id, name, age, gender, color, temperament, alive FROM bears WHERE name IS NULL;"
+# Select * From bears Where name is NULL; Why dont you work?
 end
